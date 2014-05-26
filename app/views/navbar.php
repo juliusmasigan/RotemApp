44a4935b91b87ee43b5eb98c1844875654ff66b1
Krizzel Jane <kj.bigol@dot.ph>
2014-05-26 21:18:59 +0800

    Create navbar.php

---
+++ app/views/navbar.php
@@ -0,0 +1,16 @@
+<div id="navbar" data-spy="affix" data-offset-top="200">
+        <ul class="nav nav-pills">
+          <li class="<?php echo ($page == "dashboard" ? "active" : "")?>"><a href="dashboard.php">Dashboard</a></li>
+          <li class="<?php echo ($page == "messages" ? "active" : "")?>"><a href="#">Messages</a></li>
+          <li class="<?php echo ($page == "reports" ? "active" : "")?>"><a href="reports.php">Reports</a></li>
+          <li class="<?php echo ($page == "media" ? "active" : "")?>"><a href="#">Media</a></li>
+          <li class="<?php echo ($page == "events" ? "active" : "")?>">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Events<span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Post an Event</a></li>
+                  <li><a href="#">View All Events</a></li>
+                </ul></li>
+          <li class="<?php echo ($page == "alerts" ? "active" : "")?>"><a href="#">Alerts</a></li>
+        </ul>
+</div>
