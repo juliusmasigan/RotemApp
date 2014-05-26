da64391ce16c26e11578ffe559c72ccc96a90aa4
Krizzel Jane <kj.bigol@dot.ph>
2014-05-26 21:19:40 +0800

    Create header-li.php

---
+++ app/views/header-li.php
@@ -0,0 +1,28 @@
+<!--if logged in-->
+
+<div class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">SkillQuest</a>
+        </div>
+        <div class="collapse navbar-collapse">
+           <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hi, <span>User!</span> <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">My Profile</a></li>
+                <li><a href="#">Settings</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Log out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
