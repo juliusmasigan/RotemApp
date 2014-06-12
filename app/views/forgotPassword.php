a109d7b390bcfefbccf42c8b34959ffdf830250b
KJane <Krizzel@Krizzels-MacBook-Air.local>
2014-06-12 18:59:30 +0800

    Updates

---
+++ app/views/forgotPassword.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="forgotPassword" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <form>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Forgot Password</h4>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <label for="inputRecipient">Enter Username:</label>
+          <input type="text" class="form-control" id="inputRecipient" placeholder="">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ app/views/users/register.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('body-section')
+
+
+{{ Form::open(array('url' => "/register",'method' => "post", 'class' => "section form-register")) }}
+            <h2 class="form-register-heading">Register</h2>
+            {{ Form::text('name', null, array('class' => "form-control", 'placeholder' => "Name")) }}
+
+            {{ Form::text('parentsName', null, array('class' => "form-control", 'placeholder' => "Parent's Name")) }}
+
+            {{ Form::text('mNumber', null, array('class' => "form-control", 'placeholder' => "Mobile Number")) }}
+
+            {{ Form::email('email', null, array('class' => "form-control", 'placeholder' => "Email")) }}
+
+            {{ Form::text('username', null, array('class' => "form-control", 'placeholder' => "Username")) }}
+
+            {{ Form::password('password', array('class' => "form-control", 'placeholder' => "Password")) }}
+
+            {{ Form::password('confpassword', array('class' => "form-control", 'placeholder' => "Confirm Password")) }}
+
+            {{ Form::select('userType', array('key' => "Student", 'value' => "Teacher"), 'key', array('class' => "form-control")) }}
+
+            <br>
+            {{ Form::submit('Submit', array('class' => 'btn btn-block btn-primary btn-lg')) }}
+
+{{ Form::close() }}
+
+
+@stop
