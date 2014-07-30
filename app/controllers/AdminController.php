2b8ce8650f5d8c9e899178d214429117539806ee
= <=>
2014-07-30 19:44:49 +0800

    Administrator registration.

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -10,6 +10,22 @@ class AdminController extends \BaseController {
 			'institution' => 'required',
 			'phone' => 'required'
 		);
+		$validator = Validator::make($posts, $rules);
+		$validator->setAttributeNames(array(
+			'institution' => "Institution",
+			'phone' => "Phone Number"
+		));
+		if($validator->fails()) {
+			return Redirect::to('register')->withErrors($validator)->withInput($posts);
+		}
+
+		//New Administator model instance.
+		$administrator = new Administrator;
+
+		$record = $administrator->insert(array(
+			'domain' => $posts['domain'],
+			'contact_number' => $posts['phone'],
+		));
 	}
 
 }
