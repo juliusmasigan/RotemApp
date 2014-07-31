9ea496114fa5436166efb8ed97fd2a0b14ae0712
= <=>
2014-07-31 19:18:26 +0800

    Updates.

--- app/views/admin/register.blade.php
+++ app/views/admin/register.blade.php
@@ -3,31 +3,28 @@
 @section('body-section')
 
 @if(!Session::has('pre_registration'))
-{{ Form::open(array('url' => "/pre_register", 'method' => "POST", 'class' => "section form-register")) }}
+{{ Form::open(array('url' => "/register", 'method' => "POST", 'class' => "section form-register")) }}
             <h2 class="form-register-heading">Register</h2>
             {{ Form::text('institution', null, array('class' => "form-control", 'placeholder' => "Institution", 'required')) }}
-
             {{ Form::text('domain', null, array('class' => "form-control", 'placeholder' => "company.skillquest.com")) }}
-
             {{ Form::text('phone', null, array('class' => "form-control", 'placeholder' => "Phone", 'required')) }}
-
             {{ Form::text('numberStudents', null, array('class' => "form-control", 'placeholder' => "Number of Students")) }}
             <br>
             {{ Form::submit('Submit', array('class' => 'btn btn-block btn-primary btn-lg')) }}
-
 {{ Form::close() }}
 @else
-{{ Input::get('institution') }}
-{{ Form::open(array('url' => "/register", 'method' => "POST", 'class' => "section form-register")) }}
+{{ Form::open(array('url' => "/post_register", 'method' => "POST", 'class' => "section form-register")) }}
             <h2 class="form-register-heading">Register</h2>
             {{ Form::text('fullName', null, array('class' => "form-control", 'placeholder' => "Full Name", 'required')) }}
-
             {{ Form::text('email', null, array('class' => "form-control", 'placeholder' => "Email", 'required')) }}
-
             {{ Form::text('password', null, array('class' => "form-control", 'placeholder' => "Password", 'required')) }}
             <br>
-            {{ Form::submit('Submit', array('class' => 'btn btn-block btn-primary btn-lg')) }}
 
+			{{ Form::hidden('institution', Input::old('institution')) }}
+			{{ Form::hidden('domain', Input::old('domain')) }}
+			{{ Form::hidden('phone', Input::old('phone')) }}
+			{{ Form::hidden('numberStudents', Input::old('numberStudents')) }}
+            {{ Form::submit('Submit', array('class' => 'btn btn-block btn-primary btn-lg')) }}
 {{ Form::close() }}
 @endif
 
