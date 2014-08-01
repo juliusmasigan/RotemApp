30586a050be2f00fc5792660247aff18f65121d4
= <=>
2014-08-01 19:48:41 +0800

    Updates.

--- app/controllers/TeacherController.php
+++ app/controllers/TeacherController.php
@@ -19,7 +19,7 @@ class TeacherController extends \BaseController {
 			'password' => 'Password',
 		));
 
-		$not_match = ($posts['password'] != $posts['confirmPassword'])?true:false;
+		$not_match = ($posts['password'] != $posts['confirmPassword']) ? true:false;
 		if($validator->fails() || $not_match) {
 			$errors = $validator->errors();
 			if($not_match) {
@@ -28,6 +28,16 @@ class TeacherController extends \BaseController {
 
 			return Redirect::to('teachers/register')->withErrors($errors)->withInput($posts);
 		}
+
+		$ver_code = Registration::gencode();
+
+		//Send a confirmation email.
+		Mail::send('email.registration', array(), function($message) use($posts) {
+			$message->to($posts['email'], $posts['name']);
+			$message->subject('Skillquest Registration Confirmation');
+		});
+
+		return Redirect::to('teachers/login');
 	}
 
 }
