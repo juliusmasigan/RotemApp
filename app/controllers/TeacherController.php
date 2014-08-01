6679e3c59cd45cb770c1e155162f91416bb71a6f
juliusmasigan <juliusmasigan@gmail.com>
2014-08-01 20:35:24 +0800

    Updates.

--- app/controllers/TeacherController.php
+++ app/controllers/TeacherController.php
@@ -6,38 +6,65 @@ class TeacherController extends \BaseController {
 		$posts = Input::all();
 
 		$rules = array(
-			'name' => 'required',
+			'fullName' => 'required',
 			'email' => 'required',
 			'phone' => 'required|numeric',
 			'password' => 'required',
 		);
 		$validator = Validator::make($posts, $rules);
 		$validator->setAttributeNames(array(
-			'name' => 'Name',
+			'fullName' => 'Full name',
 			'email' => 'Email address',
 			'phone' => 'Phone',
 			'password' => 'Password',
 		));
 
+		//Check if email is already registered.
+		$teachers = Teacher::where('email', $posts['email'])->get();
+
 		$not_match = ($posts['password'] != $posts['confirmPassword']) ? true:false;
-		if($validator->fails() || $not_match) {
+		if($validator->fails() || $not_match || count($teachers)) {
 			$errors = $validator->errors();
 			if($not_match) {
 				$errors->add('password', 'The Password doesn\'t match.');
+			}
+			if(count($teachers)) {
+				$errors->add('email', 'The Email is already registered.');
 			}
 
 			return Redirect::to('teachers/register')->withErrors($errors)->withInput($posts);
 		}
 
+		//Generate random verification code.
 		$ver_code = Registration::gencode();
+
+		//New Teacher model instance.
+		$teacher = new Teacher;
+
+		//New Registration model instance.
+		$registration = new Registration;
+
+		$teacher_id = $teacher->insertGetId(array(
+			'full_name' => $posts['fullName'],
+			'contact_number' => $posts['phone'],
+			'email' => $posts['email'],
+			'password' => md5($posts['password']),
+		));
+
+		$registration->insert(array(
+			'user_id' => $teacher_id,
+			'registration_type' => 'teacher',
+			'verification_code' => $ver_code,
+			'status' => 'pending',
+		));
 
 		//Send a confirmation email.
 		Mail::send('email.registration', array(), function($message) use($posts) {
-			$message->to($posts['email'], $posts['name']);
+			$message->to($posts['email'], $posts['fullName']);
 			$message->subject('Skillquest Registration Confirmation');
 		});
 
-		return Redirect::to('teachers/login');
+		return Redirect::to('/');
 	}
 
 }
--- app/views/teacher/register.blade.php
+++ app/views/teacher/register.blade.php
@@ -4,7 +4,7 @@
 
 {{ Form::open(array('url' => "teacher/register", 'method' => "POST", 'class' => "section form-register")) }}
             <h2 class="form-register-heading">Register</h2>
-            {{ Form::text('name', null, array('class' => "form-control", 'placeholder' => "Name", 'required')) }}
+            {{ Form::text('fullName', null, array('class' => "form-control", 'placeholder' => "Name", 'required')) }}
             {{ Form::text('email', null, array('class' => "form-control", 'placeholder' => "Email", 'required')) }}
             {{ Form::text('phone', null, array('class' => "form-control", 'placeholder' => "Phone", 'required')) }}
             {{ Form::password('password', array('class' => "form-control", 'placeholder' => "Password", 'required')) }}
