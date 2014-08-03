9a4e1bfe018c77a607c992915d4372898459d1de
juliusmasigan <juliusmasigan@gmail.com>
2014-08-03 09:50:46 +0800

    Student's registration confirmation page.

---
+++ app/views/student/confirm.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('body-section')
+
+	<div class="confirmation-container">
+	{{ Form::open(array('url' => "student/confirm", 'method' => 'POST', 'class' => 'section form-register')) }}
+		<h2>Verify Account</h2>
+		<p>Please enter the verification code sent in your phone.</p><br/>
+		{{ Form::text('verification_code', null, array('required', 'class' => 'form-control', 'placeholder' => 'Verification Code')) }}
+		{{ Form::hidden('user', $user) }}
+		{{ Form::hidden('key', $key) }}
+		{{ Form::submit('Submit', array('class' => 'btn btn-block btn-primary btn-lg')) }}
+	{{ Form::close() }}
+	</div>
+
+@stop
