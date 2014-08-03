22fcb4835a51a7c78680167ee6222ee4a91cb196
juliusmasigan <juliusmasigan@gmail.com>
2014-08-03 09:52:53 +0800

    Added method for generating verification code and verification link.

--- app/models/Registration.php
+++ app/models/Registration.php
@@ -6,12 +6,21 @@ class Registration extends Eloquent {
 
 	public $incrementing = true;
 
+	public $timestamps = false;
+
 	public static function gencode() {
 		list($sec, $usec) = explode(" ", microtime());
 		$seed = (float) $sec + ((float) $usec * 100000);
 		mt_srand($seed);
 		
 		return str_pad(mt_rand(), 10, '1', STR_PAD_BOTH);
+	}
+
+	public static function confirm_link($args = NULL, $user_type) {
+		if(!is_null($args) || !empty($args)) {
+			$hash = md5($args['email']."-".$args['code']."-".$args['password']);
+			return link_to("{$user_type}/confirm/{$args['email']}/{$hash}", null);
+		}
 	}
 
 }
