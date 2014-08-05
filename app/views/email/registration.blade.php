2eb0cf1379817930b0d8a23dd0cf68b410437bb7
juliusmasigan <juliusmasigan@gmail.com>
2014-08-05 15:09:27 +0800

    Displays the verification code.

--- app/views/email/registration.blade.php
+++ app/views/email/registration.blade.php
@@ -1,3 +1,6 @@
 <strong>Please click the link below to confirm your registration:</strong>
 <br /><br />
 {{ $link }}
+<br/><br/>
+<strong>Verification Code is:</strong>&nbsp;{{ $verification_code }}
+
