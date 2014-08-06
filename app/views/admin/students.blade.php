f24cf5d2f2b4a9f4d3522c595de3a44763fa5b5b
juliusmasigan <juliusmasigan@gmail.com>
2014-08-06 20:42:18 +0800

    Students lists view.

---
+++ app/views/admin/students.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+
+
+@section('body-section')
+
+<div class="clear"></div>
+<div class="container wrap">
+
+	@include('navbar')
+
+	<div class="section recordsTablewrap">
+		<table class="table table-bordered table-hover table-striped">
+			<thead>
+				<th>#</th>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Status</th>
+			</thead>
+			<tbody>
+				@foreach($students as $student)
+					@if($student->registration[0]->status == "pending")
+					<?php $row_class = "warning"; ?>
+					@elseif($student->registration[0]->status == "approval")
+					<?php $row_class = "info"; ?>
+					@endif
+					<tr class="<?php echo $row_class; ?>">
+						<td>{{ $student->id; }}</td>
+						<td>{{ $student->full_name; }}</td>
+						<td>{{ $student->email; }}</td>
+						<td>{{ $student->registration[0]->status; }}</td>
+					</tr>
+				@endforeach
+			<tbody>
+		</table>
+	</div>
+
+</div>
+
+@stop
