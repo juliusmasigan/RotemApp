9e258baf2a29439f7a5c50af2ca66f99c6c83a8c
juliusmasigan <juliusmasigan@gmail.com>
2014-08-07 08:23:02 +0800

    Modifications

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -1,9 +1,9 @@
 <div id="navbar" data-spy="affix" data-offset-top="200">
         <ul class="nav nav-pills">
-          <li class="<?php echo ($page == "dashboard" ? "active" : "")?>"><a href="dashboard">Dashboard</a></li>
-          <li class="<?php echo ($page == "messages" ? "active" : "")?>"><a href="messages">Messages</a></li>
-          <li class="<?php echo ($page == "reports" ? "active" : "")?>"><a href="reports">Reports</a></li>
-          <li class="<?php echo ($page == "media" ? "active" : "")?>"><a href="media">Media</a></li>
+          <li class="<?php echo ($page == "dashboard" ? "active" : "")?>"><a href="/dashboard">Dashboard</a></li>
+          <li class="<?php echo ($page == "messages" ? "active" : "")?>"><a href="/messages">Messages</a></li>
+          <li class="<?php echo ($page == "reports" ? "active" : "")?>"><a href="/reports">Reports</a></li>
+          <li class="<?php echo ($page == "media" ? "active" : "")?>"><a href="/media">Media</a></li>
           <li class="<?php echo ($page == "events" ? "active" : "")?>">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Events<span class="caret"></span>
                 </a>
@@ -13,8 +13,8 @@
                 </ul></li>
           <li class="<?php echo ($page == "alerts" ? "active" : "")?>"><a href="#">Alerts</a></li>
 		  @if(Session::get('user.type')[0] == 'admin')
-		  <li class="<?php echo ($page == "staffs" ? "active" : "")?>"><a href="staffs">Staffs</a></li>
-		  <li class="<?php echo ($page == "students" ? "active" : "")?>"><a href="students">Students</a></li>
+		  <li class="<?php echo ($page == "staffs" ? "active" : "")?>"><a href="/teachers">Staffs</a></li>
+		  <li class="<?php echo ($page == "students" ? "active" : "")?>"><a href="/students">Students</a></li>
 		  @endif
         </ul>
 </div>
