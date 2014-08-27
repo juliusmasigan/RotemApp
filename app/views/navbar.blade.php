f73411da01b22890bd8a447947b8e1de7556f7d3
juliusmasigan <juliusmasigan@gmail.com>
2014-08-27 18:58:09 +0800

    Made a link for the query page.

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <div id="navbar" data-spy="affix" data-offset-top="200">
         <ul class="nav nav-pills">
           <li class="<?php echo ($page == "dashboard" ? "active" : "")?>"><a href="/dashboard">Dashboard</a></li>
-          <li class="<?php echo ($page == "messages" ? "active" : "")?>"><a href="/messages">Messages</a></li>
+          <!--li class="<?php echo ($page == "messages" ? "active" : "")?>"><a href="/messages">Messages</a></li>
           <li class="<?php echo ($page == "reports" ? "active" : "")?>"><a href="/reports">Reports</a></li>
           <li class="<?php echo ($page == "media" ? "active" : "")?>"><a href="/media">Media</a></li>
           <li class="<?php echo ($page == "events" ? "active" : "")?>">
@@ -11,7 +11,8 @@
                   <li><a href="#">Post an Event</a></li>
                   <li><a href="#">View All Events</a></li>
                 </ul>
-          </li>
+          </li-->
+			<li class="<?php echo ($page == "queries" ? "active" : "")?>"><a href="/queries">Queries</a></li>
 			<?php $user_type_array = Session::get('user.type'); ?>
           	@if($user_type_array[0] != 'student')
           	<li class="<?php echo ($page == "alerts" ? "active" : "")?>"><a href="/alerts">Alerts</a></li>
